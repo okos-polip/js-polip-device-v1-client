349feff9dd0e6be0b34b65e09d50376a60375cd4
Curt Henrichs <curthenrichs@gmail.com>
2023-04-30 12:06:07 -0700

    adding comments to codebase

--- src/PolipDevice.js
+++ src/PolipDevice.js
@@ -18,16 +18,20 @@ class PolipDevice {
         firmware = null,
         rollover = null
     ) {
-        this.url = url;
-        this.value = value;             // Incremented value for next transmission id
-        this.skipTagCheck = skipTagCheck; // Set true if key -> tag gen not needed
-        this.serial = serial;     // Serial identifier unique to this device
-        this.key = key;           // Revocable key used for tag gen
-        this.hardware = hardware; // Hardware version to report to server
-        this.firmware = firmware; // Firmware version to report to server
-        this.rollover = rollover;
+        this.url = url;                     // Ingest URL (by default set to secure fixed api name)
+        this.value = value;                 // Incremented value for next transmission id
+        this.skipTagCheck = skipTagCheck;   // Set true if key -> tag gen not needed
+        this.serial = serial;               // Serial identifier unique to this device
+        this.key = key;                     // Revocable key used for tag gen
+        this.hardware = hardware;           // Hardware version to report to server
+        this.firmware = firmware;           // Firmware version to report to server
+        this.rollover = rollover;           // Defines integer rollover (for fixed width devices)
     }
 
+    /**
+     * GET request to status endpoint
+     * @returns true if reaches end of function else throws error
+     */
     async checkServerStatus() {
         try {
             const response = await axios.get(this.url + "/api/v1/");
@@ -37,8 +41,31 @@ class PolipDevice {
         } catch (error) {
             throw new Error('Failed to check server status: ' + error.message);
         }
+
+        return true;
     }
 
+    /**
+     * POST poll request
+     * 
+     * Requests data from server according to the query param list provided
+     * 
+     * This call will perform request tag generation (if device configured) and
+     * response tag match (if device configured). Will throw errors if
+     *  - server fails to communicate (axios error)
+     *  - value invalid (polip protocol error)
+     *  - tag match fail (most likely key / tag check settings mismatch)
+     * 
+     * When a value error is encountered, need to call getValue() to refresh.
+     * Then recall this method.
+     * 
+     * @param {*} state boolean flag to retrieve device state data
+     * @param {*} meta boolean flag to retrieve device meta data
+     * @param {*} sensors boolean flag to retrieve sensor meta data
+     * @param {*} rpc boolean flag to retrieve pending RPCs list
+     * @param {*} manufacturer boolean flag to retrieve manufacturer defined data
+     * @returns fully formed response JSON data with requested data from server
+     */
     async getState(state = true, meta = false, sensors = false, rpc = false, manufacturer = false) {
 
         const params = new URLSearchParams({
@@ -59,6 +86,24 @@ class PolipDevice {
         return res;
     }
 
+    /**
+     * POST push state request
+     * 
+     * Pushes state data to server. State should be compliant with schema for
+     * device.
+     * 
+     * This call will perform request tag generation (if device configured) and
+     * response tag match (if device configured). Will throw errors if
+     *  - server fails to communicate (axios error)
+     *  - value invalid (polip protocol error)
+     *  - tag match fail (most likely key / tag check settings mismatch)
+     * 
+     * When a value error is encountered, need to call getValue() to refresh.
+     * Then recall this method.
+     * 
+     * @param {} stateObj object with properly formated state representation 
+     * @returns fully formed response JSON data - acknowledgement
+     */
     async pushState(stateObj) {
         if (typeof stateObj !== 'object' && stateObj === null) {
             throw new Error('Invalid parameterization: sensor object must be provided');
@@ -74,10 +119,33 @@ class PolipDevice {
         return res;
     }
 
+    /**
+     * Wrapper around push error for notification messages (errorCode = 0)
+     * @param {*} message string message to be provided with error - should be human readable
+     */
     async pushNotification(message) {
         this.pushError(message, 0);
     }
 
+    /**
+     * POST push error (and notification) request
+     * 
+     * Pushes a message to the server (message will be treated as an urgent request 
+     * unlike state update). Up to user code to make use of this feature.
+     * 
+     * This call will perform request tag generation (if device configured) and
+     * response tag match (if device configured). Will throw errors if
+     *  - server fails to communicate (axios error)
+     *  - value invalid (polip protocol error)
+     *  - tag match fail (most likely key / tag check settings mismatch)
+     * 
+     * When a value error is encountered, need to call getValue() to refresh.
+     * Then recall this method.
+     * 
+     * @param {*} message string message to be provided with error - should be human readable
+     * @param {*} errorCode integer code (as defined by the error semantic tables / descriptions)
+     * @returns fully formed response JSON data - acknowledgement
+     */
     async pushError(message, errorCode) {
         message = String(message);
         errorCode = parseInt(errorCode);
@@ -92,6 +160,24 @@ class PolipDevice {
         return res;
     }
 
+    /**
+     * POST request push sensors
+     * 
+     * Pushes sensor data to server
+     * 
+     * This call will perform request tag generation (if device configured) and
+     * response tag match (if device configured). Will throw errors if
+     *  - server fails to communicate (axios error)
+     *  - value invalid (polip protocol error)
+     *  - tag match fail (most likely key / tag check settings mismatch)
+     * 
+     * When a value error is encountered, need to call getValue() to refresh.
+     * Then recall this method.
+     * 
+     * @param {*} sensorsObj key-value pair of subset of valid sensor keys 
+     *                       (list provided in poll-sensors / schema)
+     * @returns fully formed response JSON data - acknowledgement
+     */
     async pushSensors(sensorsObj) {
         if (typeof sensorsObj !== 'object' && sensorsObj === null) {
             throw new Error('Invalid parameterization: sensor object must be provided');
@@ -105,6 +191,18 @@ class PolipDevice {
         return res;
     }
 
+    /**
+     * POST request get value
+     * 
+     * This method should be called when a value mismatch occurs and/or at startup
+     * 
+     * This method does not follow normal value update rules (instead sets internal
+     * device value to the response value). This method does not perform tag checks
+     * 
+     * Will throw error only if axios error or server encounters error
+     * 
+     * @returns fully formed response JSON data - acknowledgement
+     */
     async getValue() {
         const res = await _requestTemplate(
             this.url + "/api/v1/device/value",
@@ -120,6 +218,24 @@ class PolipDevice {
         return res;
     }
 
+    /**
+     * POST request push completed RPC
+     * 
+     * Called when RPC is finished on device and needs to report state to server
+     * 
+     * This call will perform request tag generation (if device configured) and
+     * response tag match (if device configured). Will throw errors if
+     *  - server fails to communicate (axios error)
+     *  - value invalid (polip protocol error)
+     *  - tag match fail (most likely key / tag check settings mismatch)
+     * 
+     * When a value error is encountered, need to call getValue() to refresh.
+     * Then recall this method.
+     * 
+     * @param {uuid, result} rpcObj object with uuid as identifier string from poll
+     *                       and result as string with arbitrary form
+     * @returns fully formed response JSON data - acknowledgement
+     */
     async pushRPC(rpcObj) {
         if (rpcObj.uuid === undefined || rpcObj.uuid === null) {
             throw new Error('Invalid parameterization: RPC must have uuid');
@@ -137,6 +253,21 @@ class PolipDevice {
         return res;
     }
 
+    /**
+     * POST request schema
+     * 
+     * This call will perform request tag generation (if device configured) and
+     * response tag match (if device configured). Will throw errors if
+     *  - server fails to communicate (axios error)
+     *  - value invalid (polip protocol error)
+     *  - tag match fail (most likely key / tag check settings mismatch)
+     * 
+     * When a value error is encountered, need to call getValue() to refresh.
+     * Then recall this method.
+     * 
+     * @returns fully formed response JSON data - schema for device, state, 
+     *          sensor, and rpc key tables
+     */
     async getSchema() {
         const res = await this._requestTemplate(
             this.url + '/api/v1/device/schema', 
@@ -148,6 +279,25 @@ class PolipDevice {
         return res;
     }
 
+    /**
+     * POST Requests error semantic description and range
+     * 
+     * If code not supplied provides all specific code semantics and 
+     * error code ranges. However if a code is valid, provides single
+     * description and single range for that code.
+     * 
+     * This call will perform request tag generation (if device configured) and
+     * response tag match (if device configured). Will throw errors if
+     *  - server fails to communicate (axios error)
+     *  - value invalid (polip protocol error)
+     *  - tag match fail (most likely key / tag check settings mismatch)
+     * 
+     * When a value error is encountered, need to call getValue() to refresh.
+     * Then recall this method.
+     * 
+     * @param {*} code integer error code to be looked up (optional)
+     * @returns fully formed response JSON data - range table and descriptions
+     */
     async getErrorSemantic(code) {
         let params = (code !== undefined) ? `?code=${code}` : '';
 
@@ -161,6 +311,24 @@ class PolipDevice {
         return res;
     }
 
+    /**
+     * Private utility method
+     * 
+     * This method packs request per polip protocol, generates tag (if device configured),
+     * POSTs to endpoint, performs tag match on response (if device configured), and 
+     * increments value (with rollover if configured).
+     * 
+     * Throws errror if
+     *  - axios communication error with server
+     *  - server returns status != 200
+     *      - value mismatch error if specifically indicated
+     * 
+     * @param {*} endpoint valid URI for okospolip
+     * @param {*} reqObj request object with proper parameters filled in. Default empty
+     * @param {*} skipValue override value inclusion in request / update on response
+     * @param {*} skipTag override adding tag in request / tag check in response
+     * @returns response data object from server - tag validated
+     */
     async _requestTemplate(endpoint, reqObj = {}, skipValue = false, skipTag = false) {
         reqObj.serial = this.serial;
         reqObj.firmware = this.firmware;
--- src/const.js
+++ src/const.js
@@ -1,9 +1,16 @@
-// Fixed device ingest server URL 
+// Fixed device ingest server DNS name 
 const POLIP_DEVICE_INGEST_SERVER_ADDRESS = "api.okospolip.com";
+
+// Unsecure HTTP port
 const POLIP_DEVICE_INGEST_SERVER_HTTP_PORT = 3021;
+
+// Secure HTTPS port
 const POLIP_DEVICE_INGEST_SERVER_HTTPS_PORT = 3022;
 
+// ingest URL unsecure (use only if state data is not sensitive)
 const POLIP_DEVICE_INGEST_SERVER_URL = `http://${POLIP_DEVICE_INGEST_SERVER_ADDRESS}:${POLIP_DEVICE_INGEST_SERVER_HTTP_PORT}`;
+
+// ingest URL secure
 const POLIP_DEVICE_INGEST_SERVER_URL_SECURE = `https://${POLIP_DEVICE_INGEST_SERVER_ADDRESS}:${POLIP_DEVICE_INGEST_SERVER_HTTPS_PORT}`;
 
 // Periodic poll of server device state
